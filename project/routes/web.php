f187710f5a0be71d19a6ddc7840231249f120d90
Patryk Przybylski <patrykprzybylski92@gmail.com>
2020-05-05 00:27:19 +0200

    Add auth middleware

--- project/routes/web.php
+++ project/routes/web.php
@@ -11,24 +11,31 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/companies', 'CompaniesController@index')->name('companies.index');
-Route::get('/companies/create', 'CompaniesController@create');
-Route::post('/companies/create','CompaniesController@store')->name('companies.store');
-Route::get('/companies/edit/{id}', 'CompaniesController@edit')->name('companies.edit');
-Route::post('/companies/update', 'CompaniesController@update')->name('companies.update');
-Route::get('/companies/delete/{id}', 'CompaniesController@destroy')->name('companies.destroy');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', 'CompaniesController@index')->name('companies.index');
 
 
-Route::get('/certificates', 'CertificatesController@index')->name('certificates.index');
-Route::get('/certificates/create', 'CertificatesController@create');
-Route::post('/certificates/create','CertificatesController@store')->name('certificates.store');
-Route::get('/certificates/edit/{id}', 'CertificatesController@edit')->name('certificates.edit');
-Route::post('/certificates/update', 'CertificatesController@update')->name('certificates.update');
-Route::get('/certificates/delete/{id}', 'CertificatesController@destroy')->name('certificates.destroy');
+    Route::get('/companies', 'CompaniesController@index')->name('companies.index');
+    Route::get('/companies/create', 'CompaniesController@create');
+    Route::post('/companies/create','CompaniesController@store')->name('companies.store');
+    Route::get('/companies/edit/{id}', 'CompaniesController@edit')->name('companies.edit');
+    Route::post('/companies/update', 'CompaniesController@update')->name('companies.update');
+    Route::get('/companies/delete/{id}', 'CompaniesController@destroy')->name('companies.destroy');
+
+
+    Route::get('/certificates', 'CertificatesController@index')->name('certificates.index');
+    Route::get('/certificates/create', 'CertificatesController@create');
+    Route::post('/certificates/create','CertificatesController@store')->name('certificates.store');
+    Route::get('/certificates/edit/{id}', 'CertificatesController@edit')->name('certificates.edit');
+    Route::post('/certificates/update', 'CertificatesController@update')->name('certificates.update');
+    Route::get('/certificates/delete/{id}', 'CertificatesController@destroy')->name('certificates.destroy');
+
+    Route::get('/home', 'CompaniesController@index')->name('companies.index');
+
+});
+
+
